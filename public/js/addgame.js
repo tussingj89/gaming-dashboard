3836914fd44b3373139486d76ef00d0e57285b91
tussingj89 <tussing40@gmail.com>
2021-02-24 17:48:58 -0500

    add

---
+++ public/js/addgame.js
@@ -0,0 +1,79 @@
+$(document).ready(() => {
+  // Getting jQuery references to the post body, title, form, and author select
+  const reviewInput = $("#review");
+  const ratingInput = $("#rating");
+  const titleInput = $("#title");
+  const cmsForm = $("#cms");
+  // Adding an event listener for when the form is submitted
+  $(cmsForm).on("submit", handleFormSubmit);
+  // Gets the part of the url that comes after the "?" (which we have if we're updating a post)
+  const url = window.location.search;
+  let postId;
+  let authorId;
+  // Sets a flag for whether or not we're updating a post to be false initially
+  let updating = false;
+
+  // If we have this section in our url, we pull out the post id from the url
+  // In '?post_id=1', postId is 1
+  if (url.indexOf("?post_id=") !== -1) {
+    postId = url.split("=")[1];
+    getPostData(postId, "post");
+  }
+  // Otherwise if we have an author_id in our url, preset the author select box to be our Author
+  else if (url.indexOf("?author_id=") !== -1) {
+    authorId = url.split("=")[1];
+  }
+
+  // Getting the authors, and their posts
+  getAuthors();
+
+  // A function for handling what happens when the form to create a new post is submitted
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing a body, title, or author
+    if (
+      !titleInput.val().trim() ||
+      !reviewInput.val().trim() ||
+      !ratingInput.val().trim() ||
+      !authorSelect.val()
+    ) {
+      return;
+    }
+    // Constructing a newPost object to hand to the database
+    const newPost = {
+      title: titleInput.val().trim(),
+      review: reviewInput.val().trim(),
+      rating: ratingInput.val().trim(),
+      AuthorId: authorSelect.val()
+    };
+
+    // If we're updating a post run updatePost to update a post
+    // Otherwise run submitPost to create a whole new post
+    if (updating) {
+      newPost.id = postId;
+      updatePost(newPost);
+    } else {
+      submitPost(newPost);
+    }
+  }
+
+  // Submits a new post and brings user to blog page upon completion
+  function submitPost(post) {
+    $.post("/api/games", post, () => {
+      window.location.href = "/blog";
+    });
+  }
+
+    $.get(queryUrl, data => {
+      if (data) {
+        console.log(data.AuthorId || data.id);
+        // If this post exists, prefill our cms forms with its data
+        titleInput.val(data.title);
+        reviewInput.val(data.review);
+        ratingInput.val(data.rating);
+        authorId = data.AuthorId || data.id;
+        // If we have a post with this id, set a flag for us to know to update the post
+        // when we hit submit
+        updating = true;
+      }
+    });
