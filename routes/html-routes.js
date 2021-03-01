71f9f9fa83feb71a7e86c10e6a90012e05cbb44e
tussingj89 <tussing40@gmail.com>
2021-03-01 12:16:52 -0500

    commit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -35,12 +35,6 @@ module.exports = function(app) {
     }
   });
 
-  app.get("/gamepage.html", (req, res) => {
-    if (req.user) {
-      res.render("gamepage");
-    }
-  });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members.html", isAuthenticated, (req, res) => {
